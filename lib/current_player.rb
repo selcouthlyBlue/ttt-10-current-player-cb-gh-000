b3b9381fc5d11ed330d5c70d47de33d773869be2
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 07:31:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 def turn_count(board):
   count = 0
   board.each do |position|
-    if occupied?(position){ count += 1}
+    if occupied?(board, position){ count += 1}
   end
+end
+
+def occupied?(board, position)
+  status = board[position]
+  return !status || status == " " || status == "" 
 end
